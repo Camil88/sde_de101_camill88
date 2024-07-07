802f07eefad6f980bd96813db4c5d9addbceb264
Camill <57962280+Camil88@users.noreply.github.com>
2024-07-07 22:11:22 +0200

    Update 1-basics-solutions.py

--- 03-python-for-data-engineers/1-basics-solutions.py
+++ 03-python-for-data-engineers/1-basics-solutions.py
@@ -1,1 +1,13 @@
+# Variable: A storage location identified by its name, containing some value.
+# Question: Assign a value of 10 to variable a and 20 to variable b
+# Question: Store the result of a + b in a variable c and print it. What is the result of a + b?
 
+a = 10
+b = 20
+c = a + b
+c
+30
+
+s = '  Some string '
+# Question: How do you remove the empty spaces in front of and behind the string s?
+print(s.strip())
