7bb6c58652d26748c3b94102a73ff55582aae452
Camill <57962280+Camil88@users.noreply.github.com>
2024-07-16 23:48:40 +0200

    Update 3-data-transform-solutions.py

--- 03-python-for-data-engineers/3-data-transform-solutions.py
+++ 03-python-for-data-engineers/3-data-transform-solutions.py
@@ -1,1 +1,25 @@
+# Question: How do you read data from a CSV file at ./data/sample_data.csv into a list of dictionaries?
+import csv
+
+data = []
+data_csv = './data/sample_data.csv'
+with open(data_csv, 'r') as input_data:
+  reader = csv.DictReader(input_data)
+  for row in reader:
+	data.append(row)
+
+# Question: How do you remove duplicate rows based on customer ID?
+# ANSWER: there are no duplicated rows
+unique_data = []
+customer_ids = set()
+duplicated_ids = set()
+for row in data:
+  if row['Customer_ID'] not in customer_ids:
+		unique_data.append(row)
+		customer_ids.add(row['Customer_ID'])
+	else:
+		duplicated_ids.add(row['Customer_ID'])
+
+# Question: How do you handle missing values by replacing them with 0?
+
 
