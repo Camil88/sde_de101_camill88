27c447987551e4466035ea325f37d316687db7da
Camill <57962280+Camil88@users.noreply.github.com>
2024-07-19 22:02:57 +0200

    Update 3-data-transform-solutions.py

--- 03-python-for-data-engineers/3-data-transform-solutions.py
+++ 03-python-for-data-engineers/3-data-transform-solutions.py
@@ -40,3 +40,32 @@ for row in data_unique:
   row['Surname'] = row['Customer_Name'].split()[1]
 
 # Question: How do you calculate the total purchase amount by Gender?	
+for row in data_cleaned:
+  total_purchase[row['Gender']] =+ float(row['Purchase_Amount'])
+
+# Question: How do you calculate the average purchase amount by Age group?
+# assume age_groups is the grouping we want
+# hint: Why do we convert to float?
+age_groups = {"18-30": [], "31-40": [], "41-50": [], "51-60": [], "61-70": []}
+for row in data_cleaned:
+  age = int(row['Age'])
+  if age <= 30:
+    age_groups['18-30'].append(float(row['Purchase_Amount']))
+  elif age <= 40:
+    age_groups['31-40'].append(float(row['Purchase_Amount']))
+  elif age <= 50:
+    age_groups['41-50'].append(float(row['Purchase_Amount']))
+  elif age <= 60:
+    age_groups['51-60'].append(float(row['Purchase_Amount']))
+  else:
+    age_groups['61-70'].append(float(row['Purchase_Amount']))
+
+average_purchase = {
+	group: sum(amounts) / len(amounts) for group, amounts in age_groups.items()
+}
+
+# Question: How do you print the results for total purchase amount by Gender and average purchase amount by Age group?
+print("Total purchase amount by Gender:", total_purchase)
+print("Average purchase amount by Age group:", average_purchase)
+
+# As for DuckDb: I usually use MSSQL and can do all kind of transformations through SQL pretty well so I don't touch this tasks here
